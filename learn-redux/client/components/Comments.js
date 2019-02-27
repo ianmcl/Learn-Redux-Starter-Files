c38e002ad7859f993550ed3f58070a884e5b1f40
Ian McLaughlin <imclaughlin@nyc.rr.com>
2019-02-26 21:22:36 -0500

    It's done!

---
+++ learn-redux/client/components/Comments.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const Comments = React.createClass({
+	renderComment(comment, i) {
+		return (
+			<div className="comment" key={i}>
+				<p>
+					<strong>{comment.user}</strong>
+					{comment.text}
+					<button className="remove-comment" onClick={this.props.removeComment.bind(null, this.props.params.postId, i)}>&times;</button>
+				</p>
+			</div>
+		);
+	},
+
+	handleSubmit(e) {
+		e.preventDefault();
+		const { postId } = this.props.params;
+		const author = this.refs.author.value;
+		const comment = this.refs.comment.value;
+		this.props.addComment(postId, author, comment);
+		this.refs.commentForm.reset();
+	},
+
+	render() {
+		return (
+			<div className="comments">
+				{this.props.postComments.map(this.renderComment)}
+				<form className="comment-form" ref="commentForm" onSubmit={this.handleSubmit}>
+					<input placeholder="author" ref="author" type="text" />
+					<input placeholder="comment" ref="comment" type="text" />
+					<input type="submit" hidden />
+				</form>
+			</div>
+		);
+	}
+});
+
+export default Comments;
--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,33 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+
+import store, { history } from './store';
+
+import App from './components/App';
+import PhotoGrid from './components/PhotoGrid';
+import Single from './components/Single';
+
+import css from './styles/style.styl';
+
+import Raven from 'raven-js';
+import { sentry_url } from './data/config';
+
+Raven.config(sentry_url).install();
+
+const router = (
+	<Provider store={store}>
+		<Router history={history}>
+			<Route path="/" component={App}>
+				<IndexRoute component={PhotoGrid}></IndexRoute>
+				<Route path="/view/:postId" component={Single}></Route>
+			</Route>
+		</Router>
+	</Provider>
+);
+
+render (
+	router,
+	document.getElementById('root')
+);
